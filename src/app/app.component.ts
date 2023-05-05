616b510c6c8ca24c2d8174963b42a4837e8496ea
DevJonathanSantos <jojortd0@gmail.com>
2023-05-04 23:03:23 -0300

    Especial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'Portifolio';
+
+  categorias = [
+    {
+      nome: 'Linguagens',
+      itens: ['.NET(C#)', 'TypeScript', 'JavaScript', 'Python'],
+    },
+    {
+      nome: 'Bancos de dados',
+      itens: ['SQLServer', 'MySQL', 'PostgresSQL', 'MongoDB', 'Redis'],
+    },
+    {
+      nome: 'Front-end',
+      itens: ["AngularJS","Angular","ReactJS","NextJS","HTML","CSS","Bootstrap"],
+    },
+    {
+      nome: 'Serviços AWS',
+      itens: ["S3","ECS","EC2","SNS","SQS","SES","Codecommit","CloudWatch"],
+    },
+    {
+      nome: 'Outros',
+      itens: ["RabbitMQ","Worker SoapUI","Postman","Git","Bitbucket","Visual Studio","VSCode"],
+    },
+  ];
 }
